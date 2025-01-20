cf38cd39c027cfb3fcc283b847896761901f7f84
GabeCarbone2 <carbonegabriel913@gmail.com>
2025-01-20 12:39:45 -0300

    inicializando projeto

---
+++ src/main/java/com/projeto/javaspring/JavaspringApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.javaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringApplication.class, args);
+	}
+
+}
