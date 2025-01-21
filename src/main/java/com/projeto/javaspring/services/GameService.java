7cbf21a72816ec95488eb68642da4f2ddaf169e0
GabeCarbone2 <carbonegabriel913@gmail.com>
2025-01-21 11:02:33 -0300

    GameDTO

--- src/main/java/com/projeto/javaspring/services/GameService.java
+++ src/main/java/com/projeto/javaspring/services/GameService.java
@@ -1,10 +1,12 @@
 package com.projeto.javaspring.services;
 
+import com.projeto.javaspring.dto.GameDTO;
 import com.projeto.javaspring.dto.GameMinDTO;
 import com.projeto.javaspring.entities.Game;
 import com.projeto.javaspring.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,9 +15,17 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository; //injetando um componente no outro
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
         return dto;
     }
 }
